6b4dfc9e786dbd04671579054171dfcaad761e25
Kheifer Fuller <kheifer.fuller@gmail.comgit statusgit config --global user.email kheifer.fuller@gmail.com>
2017-07-13 09:14:51 -0700

    add a limit for numbers greater than 3999

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
-//BACK-END
-
-
-//FRONT-END
 $(document).ready(function() {
+  //BACK-END
+var num = math.floor($("#usernumber").val());
 
+
+function splitNumber(num) {
+  if (num >= 4000) {
+    return alert("Please use a number between 1 and 3999");
+  } else {
+
+  }
+}
+
+  //FRONT-END
   $("#pax-romani").submit(function(event) {
     event.preventDefault();
-    
+
   })
 
 });
